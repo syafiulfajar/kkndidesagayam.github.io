a6dc8126bde31e1b28217be7bac972eca690e702
syafiulfajar <109303572+syafiulfajar@users.noreply.github.com>
2022-08-09 11:20:37 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'kkndidesagayam.github.io',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'kkndidesagayam',
